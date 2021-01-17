4a3de502a462b80519d24d956edfd03f3dae990e
ckanelin <lincandy.yite@gmail.com>
2021-01-16 16:31:17 -0800

    Add pbc download, and fix pbc upload to empty before upload

--- database/pbc.js
+++ database/pbc.js
@@ -1,12 +1,26 @@
-const handlePBCReqUpload = (req, res, db) => {
+const handlePBCUpload = (req, res, db) => {
 	const text = req.body;
-      const pbcCollections = db.collection('pbc')
+	const pbcCollections = db.collection('pbc')
 
-      pbcCollections.insertOne(text).then(results => {
+	pbcCollections.remove();
+	pbcCollections.insertMany(text).then(results => {
 		res.send('success');
-	  }).catch(error => {res.status(400).json('Cannot register user');})
+	}).catch(error => {res.status(400).json('Cannot register user');})
+
+}
+
+const handlePBCDownload = (req, res, db) => {
+	const text = req.body;
+	const pbcCollections = db.collection('pbc')
+
+	pbcCollections.find().toArray()
+	.then(results => {
+	res.send(results);
+	})
+	.catch(error => console.error(error));
 }
 
 module.exports = {
-	handlePBCReqUpload: handlePBCReqUpload
+	handlePBCUpload: handlePBCUpload,
+	handlePBCDownload: handlePBCDownload
 }
