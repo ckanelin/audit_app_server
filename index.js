70a534b2edb2d673f20517182ecd4204c8d5d2d3
ckanelin <lincandy.yite@gmail.com>
2021-01-16 19:33:50 -0800

    deploy server

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+app.listen(process.env.PORT||8080, () => {
+  console.log(`App is running`);
 });
 
 
