682c23f4cb1bd7b59560db82d39c0a56dd2d0bde
Richard <rchen2@ualberta.ca>
2021-01-17 00:49:54 -0800

    pdf.js changes

--- database/pdf.js
+++ database/pdf.js
@@ -1,42 +1,74 @@
 const AnalyzePDFs = require("../model/AnalyzePDFs.js");
 
-const handlePDFUpload = (req,res, db) => {
-    const text = req.body;
-	const pdfCollections = db.collection('pdf')
-   
-	pdfCollections.remove();
-	pdfCollections.insertMany(text).then(results => {
-		res.send('success');
-	}).catch(error => {res.status(400).json('Cannot upload pdfs');})
-}
+const handlePDFUpload = (req, res, db) => {
+  const text = req.body;
+  const pdfCollections = db.collection("pdf");
 
-async function handlePDFDownload (req,res,db) {
-    const text = req.body;
-    const pdfCollections = db.collection('pdf');
+  pdfCollections.remove();
+  pdfCollections
+    .insertMany(text)
+    .then((results) => {
+      res.send("success");
+    })
+    .catch((error) => {
+      res.status(400).json("Cannot upload pdfs");
+    });
+};
 
-    pdfCollections.find().toArray()
-	.then(results => {
-        const analyzePDFs = new AnalyzePDFs();
-        const arrayLength = results.length;
-        const sampleUploadMap = new Map();
+async function handlePDFDownload(req, res, db) {
+  const text = req.body;
+  const pdfCollections = db.collection("pdf");
 
-        for(i=0; i < arrayLength; i++){
-            sampleUploadMap.set(results[i].id,results[i].name);
-        }
+  pdfCollections
+    .find()
+    .toArray()
+    .then((results) => {
+      const analyzePDFs = new AnalyzePDFs();
+      const arrayLength = results.length;
+      const sampleUploadMap = new Map();
 
-        analyzePDFs
+      for (i = 0; i < arrayLength; i++) {
+        sampleUploadMap.set(results[i].id, results[i].name);
+      }
+
+      analyzePDFs
         .analyze(sampleUploadMap, ["Total", "Tax"])
         .then((value) => {
-            return value;
-        }).then((data)=> {
-            console.log(data);
-            res.send(JSON.stringify(data));
+          return value;
         })
+        .then((data) => {
+          const jsonObj = new Object();
+          for (value of data) {
+            jsonObj[value[0]] = Object.fromEntries(value[1]);
+          }
+          res.send(jsonObj);
+        });
     })
-	.catch(error => console.error(error));
+    .catch((error) => console.error(error));
+}
+
+function replacer(key, value) {
+  const originalObject = this[key];
+  if (originalObject instanceof Map) {
+    return {
+      dataType: "Map",
+      value: Array.from(originalObject.entries()), // or with spread: value: [...originalObject]
+    };
+  } else {
+    return value;
+  }
+}
+
+function reviver(key, value) {
+  if (typeof value === "object" && value !== null) {
+    if (value.dataType === "Map") {
+      return new Map(value.value);
+    }
+  }
+  return value;
 }
 
 module.exports = {
-    handlePDFUpload: handlePDFUpload,
-    handlePDFDownload: handlePDFDownload
-}
+  handlePDFUpload: handlePDFUpload,
+  handlePDFDownload: handlePDFDownload,
+};
